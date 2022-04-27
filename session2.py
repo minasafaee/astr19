61c37dc8b84989f6804143c33f422a72c22d32cc
minasafaee <102757978+minasafaee@users.noreply.github.com>
2022-04-26 17:04:40 -0700

    Add files via upload

---
+++ session2.py
@@ -0,0 +1,11 @@
+a = 1.2 + 2.3
+print ("1.2 + 2.3 = ", a)
+print ("data type: " + str(type(a)) + "\n")
+
+b = 2 - 1
+print ("2 - 1 = ", b)
+print ("data type: " + str(type(b)) + "\n")
+
+c = 1.5 * 2
+print ("1.5 * 2 = ", c)
+print ("data type: " + str(type(c)))
